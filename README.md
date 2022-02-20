f93fda720f5bf0a034e089793057c7fd2dc97c2e
Robin Scherrer <mail@robinscherrer.com>
2022-02-20 14:00:43 +0100

    Add flag for printing current version

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A small CLI tool to generate TOTP (Time-based One-time Password) tokens.
 
 ```
   -s, --secretkey string   supply a valid TOTP secret key to generate a token from
+  -v, --version            print version and exit
 ```
--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	flag "github.com/spf13/pflag"
 	"github.com/xlzd/gotp"
 )
 
+const version = "2.0.1"
+
 func main() {
 	secretKey := flag.StringP("secretkey", "s", "", "supply a valid TOTP secret key to generate a token from")
+	printVersion := flag.BoolP("version", "v", false, "Print the current version and exit")
 	flag.Parse()
+
+	if *printVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	if *secretKey == "" {
 		log.Fatal("secretkey cannot be empty")
 	}
